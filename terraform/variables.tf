edfc1426b1ec38459c7e7628d6f278677fe8d383
implodingduck <zsek320@gmail.com>
2021-11-29 21:02:44 -0600

    updating variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,25 @@
+variable "subscription_id" {
+  type = string
+  sensitive = true
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+  sensitive = true
+}
+
+variable "key" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
+
 variable "location" {
   type    = string
   default = "East US"
