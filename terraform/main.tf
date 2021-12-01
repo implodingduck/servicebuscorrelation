684c2024011c25be2e4393723cb52a638d126605
implodingduck <zsek320@gmail.com>
2021-11-30 21:19:21 -0600

    working on backend and security

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,9 @@ terraform {
       version = "=3.1.0"
     }
   }
+  backend "azurerm" {
+
+  }
 }
 
 # Configure the Microsoft Azure Provider
@@ -20,6 +23,13 @@ provider "azurerm" {
 locals {
   loc_for_naming = lower(replace(var.location, " ", ""))
 }
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_log_analytics_workspace" "default" {
+  name                = "DefaultWorkspace-${data.azurerm_client_config.current.subscription_id}-EUS"
+  resource_group_name = "DefaultResourceGroup-EUS"
+} 
 
 resource azurerm_resource_group "rg" {
     name = "rg-servicebus-correlation-demo-${local.loc_for_naming}"
@@ -52,6 +62,17 @@ resource "null_resource" "build_typescript"{
   }
 }
 
+resource "null_resource" "build_typescript2"{
+  triggers = {
+    index = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    working_dir = "../func2"
+    command     = "npm install && npm run build:production"
+  }
+}
+
+
 module "func1" {
     depends_on = [
       null_resource.build_typescript
@@ -63,9 +84,12 @@ module "func1" {
     working_dir = "../func1"
     app_settings = {
       "FUNCTIONS_WORKER_RUNTIME" = "node"
-      "servicebusconnectstring"  = azurerm_servicebus_namespace.correlation.default_primary_connection_string
+      "servicebusconnectstring"  = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv.name};SecretName=${azurerm_key_vault_secret.servicebusconnectstring.name})"
     }
     linux_fx_version = "node|14"
+    identity = [{ 
+      type = "System"
+    }]
 }
 
 module "func2" {
@@ -76,7 +100,83 @@ module "func2" {
     working_dir = "../func2"
     app_settings = {
       "FUNCTIONS_WORKER_RUNTIME" = "node"
-      "servicebusconnectstring"  = azurerm_servicebus_namespace.correlation.default_primary_connection_string
+      "servicebusconnectstring"  = "@Microsoft.KeyVault(VaultName=${azurerm_key_vault.kv.name};SecretName=${azurerm_key_vault_secret.servicebusconnectstring.name})"
     }
     linux_fx_version = "node|14"
+
+    identity = [{ 
+      type = "System"
+    }]
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                       = "${local.func_name}-kv"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled = false
+
+
+    
+  tags = local.tags
+}
+
+resource "azurerm_key_vault_access_policy" "client-config" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "create",
+    "get",
+    "purge",
+    "recover",
+    "delete"
+  ]
+
+  secret_permissions = [
+    "set",
+    "purge",
+    "get",
+    "list",
+    "delete"
+  ]
+
+  certificate_permissions = [
+    "purge"
+  ]
+
+  storage_permissions = [
+    "purge"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "func1" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = module.func1.identity_principal_id
+  secret_permissions = [
+    "get",
+    "list"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "func2" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = module.func2.identity_principal_id
+  secret_permissions = [
+    "get",
+    "list"
+  ]
+}
+resource "azurerm_key_vault_secret" "servicebusconnectstring" {
+  depends_on = [
+    azurerm_key_vault_access_policy.client-config
+  ]
+  name         = "servicebusconnectstring"
+  value        = azurerm_servicebus_namespace.correlation.default_primary_connection_string
+  key_vault_id = azurerm_key_vault.kv.id
 }
